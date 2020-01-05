0c3f8108b8dc69045bda4c9c6b334a8b1bfa07fc
huanglin <tomcat1969@gmail.com>
2020-01-04 23:25:27 -0800

    ready for python stack

---
+++ test111/countries.sql
@@ -0,0 +1,72 @@
+
+-- 1. What query would you run to get all the countries that speak Slovene? 
+-- Your query should return the name of the country, language and language percentage. 
+-- Your query should arrange the result by language percentage in descending order. (1)
+
+
+
+select countries.name, languages.language,languages.percentage
+from countries
+left join languages
+on countries.id = languages.country_id
+where languages.language = 'Slovene'
+order by languages.percentage desc
+
+-- 2. What query would you run to display the total number of cities for each country? 
+-- Your query should return the name of the country and the total number of cities. 
+-- Your query should arrange the result by the number of cities in descending order. (3)
+
+select countries.name as countryName,count(cities.name)
+from countries
+left join cities on countries.id = cities.country_id
+group by countries.name
+
+-- 3. What query would you run to get all the cities in Mexico with a population of greater than 500,000? 
+-- Your query should arrange the result by population in descending order. (1)
+
+select cities.name , countries.name, cities.population
+from cities
+join countries on cities.country_id = countries.id
+where countries.name = 'Mexico' and cities.population > 500000
+
+-- 4. What query would you run to get all languages in each country with a percentage greater than 89%?
+--  Your query should arrange the result by percentage in descending order. (1)
+
+select languages.language,languages.percentage,countries.name
+from languages
+left join countries on languages.country_id = countries.id
+where languages.percentage > 89
+order by languages.percentage desc
+
+-- 5. What query would you run to get all the countries with Surface Area below 501 and Population greater than 100,000? (2)
+select countries.name,countries.surface_area,countries.population
+from countries
+where countries.surface_area < 501 and countries.population > 100000
+
+--6. What query would you run to get countries 
+--with only Constitutional Monarchy with a capital greater than 200 and a life expectancy greater than 75 years? (1)
+
+select countries.name, countries.government_form,countries.indep_year,countries.capital
+from countries
+where countries.government_form like "Constitutional Monarchy" 
+and countries.capital > 200
+and year(NOW()) - countries.indep_year > 75
+
+
+-- 7. What query would you run to get all the cities of Argentina inside the Buenos Aires
+--  district and have the population greater than 500, 000? The query should return the Country Name, 
+--  City Name, District and Population. (2)
+select countries.name,cities.name,cities.district,cities.population
+from cities
+join countries on cities.country_id = countries.id
+where cities.district = "Buenos Aires" 
+and cities.population > 500000
+
+-- 8. What query would you run to summarize the number of countries in each region? 
+-- The query should display the name of the region and the number of countries. Also,
+--  the query should arrange the result by the number of countries in descending order. (2)
+select countries.region, count(countries.id) as "the number of countries"
+from countries
+group by countries.region 
+order by count(countries.id) desc
+
