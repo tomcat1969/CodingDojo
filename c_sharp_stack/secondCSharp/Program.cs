d8c2c1b53633c9edc44ed4bf3e7e0f91b2782251
huanglin <tomcat1969@gmail.com>
2020-02-02 12:09:44 -0800

    fundamentals1

---
+++ c_sharp_stack/secondCSharp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace secondCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string aa = "huang";
+            Console.WriteLine($"Hello World!{aa}");
+            Console.WriteLine("The value of pi is " + 3.14159);
+
+            for (int i =1; i <= 255; i++) {
+                Console.Write("{0} ",i);
+            }
+            Console.WriteLine("*************");
+            for (int i = 1; i <= 100; i++){
+                if(i % 3 == 0 && i % 5 == 0){
+                    continue;
+                }else if (i % 3 == 0 || i % 5 == 0) {
+                    Console.Write("{0} ",i);
+                }
+            }
+            Console.WriteLine("*****Fizz & buzz********");
+            for (int i = 1; i <= 100; i++){
+                if(i % 3 == 0 && i % 5 == 0){
+                    Console.WriteLine("{0} --- FizzBuzz",i);
+                }else if (i % 3 == 0) {
+                    Console.WriteLine("{0} ---Fizz ",i);
+                }else if (i % 5 == 0) {
+                    Console.WriteLine("{0} --- Buzz",i);
+                }
+            }
+
+
+
+
+        }
+    }
+}
