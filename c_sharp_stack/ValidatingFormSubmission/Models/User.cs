032ca3de27298d2704babc42a2d0f88c6ae6f4e5
huanglin <tomcat1969@gmail.com>
2020-02-10 08:06:00 -0800

    down with NoZ in firstname

--- c_sharp_stack/ValidatingFormSubmission/Models/User.cs
+++ c_sharp_stack/ValidatingFormSubmission/Models/User.cs
@@ -7,6 +7,7 @@ namespace ValidatingFormSubmission.Models
     {
         [Required]
         [MinLength(4)]
+        [NoZNames]
         public string Firstname { get; set; }
 
         [Required]
@@ -25,4 +26,18 @@ namespace ValidatingFormSubmission.Models
         [DataType(DataType.Password)]
         public string Password { get; set; }
     }
+
+
+
+
+
+    public class NoZNamesAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (((string)value).ToLower()[0] == 'z')
+            return new ValidationResult("No names that start with Z allowed!");
+        return ValidationResult.Success;
+    }
+}
 }
