9fab7ddfda3920baf8b945ada5ca2ce0890f9147
huanglin <tomcat1969@gmail.com>
2020-02-10 08:36:44 -0800

    done with date time validation

--- c_sharp_stack/ValidatingFormSubmission/Models/User.cs
+++ c_sharp_stack/ValidatingFormSubmission/Models/User.cs
@@ -17,6 +17,10 @@ namespace ValidatingFormSubmission.Models
         [Required]
         [Range(0,150)]
         public int Age {get;set;}
+
+        [Required]
+        [FutureDate]
+        public DateTime Birthday{get;set;}
  
         [Required]
         [EmailAddress]
@@ -32,12 +36,33 @@ namespace ValidatingFormSubmission.Models
 
 
     public class NoZNamesAttribute : ValidationAttribute
-{
-    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        if (((string)value).ToLower()[0] == 'z')
-            return new ValidationResult("No names that start with Z allowed!");
-        return ValidationResult.Success;
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (((string)value).ToLower()[0] == 'z')
+                return new ValidationResult("No names that start with Z allowed!");
+            return ValidationResult.Success;
+        }
     }
-}
+
+
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // You first may want to unbox "value" here and cast to to a DateTime variable!
+            DateTime convertedValue = Convert.ToDateTime(value);
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine("****************"+ convertedValue);
+             Console.WriteLine("****************"+ now);
+
+            if(convertedValue >= now)
+                return new ValidationResult("birth day should be in the past!");
+                 
+            return ValidationResult.Success;
+
+
+        }
+    }
 }
