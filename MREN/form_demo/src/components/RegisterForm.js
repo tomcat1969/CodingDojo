6784fdfe10c05ea64ccc1d94963979884e8fd823
huanglin <tomcat1969@gmail.com>
2020-03-05 19:15:20 -0800

    add form validation

--- MREN/form_demo/src/components/RegisterForm.js
+++ MREN/form_demo/src/components/RegisterForm.js
@@ -5,36 +5,53 @@ const RegisterForm = props => {
         lastName: "",
         email: "",
         password: "",
-        confirmPassword: ""
+        confirmPassword: "",
+        submitted: false
+
     })
     const onChangeHandler = event => {
         setFormState({
             ...formState,
-            [event.target.name]: event.target.value
+            [event.target.name]: event.target.value,
+
+            
         });
     }
     const onSubmitHandler = event => {
         event.preventDefault();
         console.log(formState);
+        setFormState({
+            ...formState,
+            submitted: true
+            
+        });
     }
+
+    let message;
+    if(formState.submitted){
+        message=<h1>You have submitted the form</h1>;
+    }else{
+        message=<h1>You have not yet submitted the form</h1>;
+    }
     return(
         <div>
+            <p>{message}</p>
             <form onSubmit={onSubmitHandler}>
                 <label>First Name</label>
-                <input type="text" name="firstName" onChange={onChangeHandler}/>
-                <br/>        
+                <input type="text" name="firstName" onChange={onChangeHandler}/> 
+                <br/> <p>{formState.firstName.length < 2 && formState.firstName.length > 0 ? <h5>first name must be at least 2 characters</h5> : <h5></h5>}</p>       
                 <label>Last Name</label>
                 <input type="text" name="lastName" onChange={onChangeHandler}/>
-                <br/>        
+                <br/>  <p>{formState.lastName.length < 2 && formState.lastName.length > 0 ? <h5>last name must be at least 2 characters</h5> : <h5></h5>}</p>      
                 <label>Email</label>
                 <input type="email" name="email" onChange={onChangeHandler}/>
-                <br/>        
+                <br/><p>{formState.email.length < 2 && formState.email.length > 0 ? <h5>email must be at least 2 characters</h5> : <h5></h5>}</p>        
                 <label>Password</label>
                 <input type="password" name="password" onChange={onChangeHandler}/>
-                <br/>        
+                <br/> <p>{formState.password.length < 8 && formState.password.length > 0 ? <h5>password must be at least 8 characters</h5> : <h5></h5>}</p>       
                 <label>Confirm Password</label>
                 <input type="password" name="confirmPassword" onChange={onChangeHandler}/>
-                <br/>   
+                <br/> <p>{formState.confirmPassword != formState.password ? <h5>passwords must match</h5> : <h5></h5>}</p>  
                 <input type="submit" />     
             </form>
             <div>
